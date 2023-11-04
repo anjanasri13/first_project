075af3abec35f4f4c7823dbace9e814ede8f385c
anjanasri13 <srianjana55@gmail.com>
2023-11-04 11:43:02 +0530

    color change

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Example from "./pages/main";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ConfigProvider theme={{ token: { colorPrimary: "red" } }}>
+    <ConfigProvider theme={{ token: { colorPrimary: "blue" } }}>
       {/* <App1 /> */}
       <Example />
     </ConfigProvider>
